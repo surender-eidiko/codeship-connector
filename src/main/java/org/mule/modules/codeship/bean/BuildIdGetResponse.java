e749637cf197dca7bec026b1cded49205415d4b4
surender-eidiko <surender.molugu@eidiko.com>
2016-05-12 15:41:09 +0530

    latest code2

--- src/main/java/org/mule/modules/codeship/bean/BuildIdGetResponse.java
+++ src/main/java/org/mule/modules/codeship/bean/BuildIdGetResponse.java
@@ -8,18 +8,48 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BuildIdGetResponse extends StatusResponse {
 	
+	/**
+	 * The build id.
+	 *
+	 */
 	private String id;
+	/**
+	 * The uuid
+	 *
+	 */
 	private String uuid;
 	
+	/**
+	 * The build id.
+	 *
+	 * @return The id
+	 */
 	public String getId() {
 		return id;
 	}
+	/**
+	 * The build id.
+	 *
+	 * @param id
+	 *            The build id
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
+	/**
+	 * The uuid.
+	 *
+	 * @return The uuid
+	 */
 	public String getUuid() {
 		return uuid;
 	}
+	/**
+	 * The uuid.
+	 *
+	 * @param uuid
+	 *            The uuid
+	 */
 	public void setUuid(String uuid) {
 		this.uuid = uuid;
 	}
